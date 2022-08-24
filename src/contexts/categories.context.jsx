02133d42c6df55559d4a4e151817eb7ecac78452
Steven Gant <sgant39@gmail.com>
2022-08-23 17:01:01 -0700

    second first commit

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+const COLLECTIONS = gql`
+  query {
+    collections {
+      id
+      title
+      items {
+        id
+        name
+        price
+        imageUrl
+      }
+    }
+  }
+`
+
+export const CategoriesProvider = ({ children }) => {
+  const {loading, error, data} = useQuery(COLLECTIONS);
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  console.log("loading", loading);
+  console.log("data", data);
+
+  const value = { categoriesMap };
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
+``
